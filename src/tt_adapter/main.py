e606192bd8f8cc545f41d44a69afdcd42220864c
Vraj Prajapati <vprajapati@tenstorrent.com>
2024-09-03 20:13:55 +0000

    Interim Update

--- src/tt_adapter/main.py
+++ src/tt_adapter/main.py
@@ -3,9 +3,13 @@
 from typing import Dict
 from model_explorer import Adapter, AdapterMetadata, ModelExplorerGraphs, graph_builder
 from collections import defaultdict
-from ttmlir import ir
+from ttmlir import ir, passes
 from ttmlir.dialects import ttkernel, tt, ttir
 from .utils import *
+from .ttir import *
+from .overrides import *
+from ttrt.common.api import API
+import os
 
 
 class TTAdapter(Adapter):
@@ -22,131 +26,63 @@ class TTAdapter(Adapter):
     def __init__(self):
         super().__init__()
 
-    def convert(self, model_path: str, settings: Dict) -> ModelExplorerGraphs:
+    def initialize(self, model_path: str, settings: Dict):
+        # Initialize machine to generate SystemDesc and load up functionality to begin
+        API.initialize_apis()
+
+        query = API.Query(args={"save_artifacts": True})
+        query()  # Save the SystemDesc to a certain localpath: './ttrt-artifacts/system_desc.ttsys'
+        os.putenv(
+            "TT_SYSTEM_DESC_PATH", f"{os.getcwd()}/ttrt-artifacts/system_desc.ttsys"
+        )
+        return {"system_desc_path": f"{os.getcwd()}/ttrt-artifacts/system_desc.ttsys"}
+
+    def execute(self, model_path: str, settings: Dict):
+        # Execute TTIR Module given model_path
+
+        # Initialize settings and TTRT API
+        API.initialize_apis()
+        ttrt_args = API.Perf.registered_args
+
+        if "ttrt_args" in settings and isinstance(settings["ttrt_args"], dict):
+            for k, v in settings["ttrt_args"].items():
+                ttrt_args[k] = v
+
+        # Initialize the SystemDesc for the PipelineOption
+        if "TT_SYSTEM_DESC_PATH" in os.environ:
+            ttir_to_ttnn_options = f'path={os.environ["TT_SYSTEM_DESC_PATH"]},'
+        ttir_to_ttnn_options += settings.get("ttir_to_ttnn_options", "")
+
+        # Created scoped context and module
         f = open(model_path, "r")
-        name_dict = defaultdict(int)
-        connections = defaultdict(int)
-        value_dict = {}
-
-        graph = graph_builder.Graph(id="ttir-graph")
-
         with ir.Context() as ctx:
             ttkernel.register_dialect(ctx)
             ttir.register_dialect(ctx)
             tt.register_dialect(ctx)
+            module = ir.Module.parse("".join(f.readlines()), ctx)
+            # Transform the module into TTNN, apply options if any
+            passes.passes.ttir_to_ttnn_backend_pipeline(module, ttir_to_ttnn_options)
+            fb_capsule = passes.passes.ttnn_to_flatbuffer_binary(module)
+            ttrt_args["capsule"] = fb_capsule
+            if "binary" in ttrt_args:
+                del ttrt_args["binary"]
+            perf_instance = API.Perf(args=ttrt_args)
 
+        perf_instance()  # Run the performance trace
+
+        return {}
+
+    def convert(self, model_path: str, settings: Dict) -> ModelExplorerGraphs:
+        f = open(model_path, "r")
+        with ir.Context() as ctx:
+            ttkernel.register_dialect(ctx)
+            ttir.register_dialect(ctx)
+            tt.register_dialect(ctx)
             module = ir.Module.parse("".join(f.readlines()), ctx)
 
-            for op in module.body.operations:
-                # High level functions, need to list their arguments in the graph
-                name_num = name_dict[get_name(op.name)]
-                id = get_name(op.name) + str(name_num)
-                namespace = get_name(op.name)
-                graph.nodes.append(
-                    graph_builder.GraphNode(id=id, label=get_name(op.name))
-                )
-                graph.nodes[-1].attrs.extend(get_attrs(op))
-                for arg in op.arguments:
-                    value_dict[arg.get_name()] = graph.nodes[-1]
+        # Apply overrides, save new module and send new model_path back.
+        overrides_process_me_settings(settings)
 
-                for region in op.regions:
-                    for block in region.blocks:
-                        for op in block.operations:
-                            # Just list out the nodes and assign their ids
-                            name_num = name_dict[get_name(op.name)]
-                            id = get_name(op.name) + str(name_num)
-                            name_dict[get_name(op.name)] += 1
-                            graph.nodes.append(
-                                graph_builder.GraphNode(
-                                    id=id, label=get_name(op.name), namespace=namespace
-                                )
-                            )
-                            graph.nodes[-1].attrs.extend(get_attrs(op))
-                            for result in op.results:
-                                # Attach the graph node here
-                                value_dict[result.get_name()] = graph.nodes[-1]
-
-                            for ops in op.operands:
-                                # Guaranteed topological ordering, so we can start to connect previous values to their respective operations
-                                source_node: graph_builder.GraphNode = value_dict[
-                                    ops.get_name()
-                                ]
-                                graph.nodes[-1].incomingEdges.append(
-                                    graph_builder.IncomingEdge(
-                                        sourceNodeId=source_node.id,
-                                        sourceNodeOutputId=str(
-                                            connections[source_node.id]
-                                        ),
-                                        targetNodeInputId=str(
-                                            len(graph.nodes[-1].incomingEdges)
-                                        ),
-                                    )
-                                )
-
-                                if (
-                                    hasattr(ops.type, "encoding")
-                                    and ops.type.encoding is None
-                                ):
-                                    source_node_attrs = [
-                                        graph_builder.KeyValue(
-                                            key="shape", value=str(ops.type.shape)
-                                        ),
-                                        graph_builder.KeyValue(
-                                            key="element_type",
-                                            value=str(ops.type.element_type),
-                                        ),
-                                        graph_builder.KeyValue(
-                                            key="rank", value=str(ops.type.rank)
-                                        ),
-                                    ]
-                                else:
-                                    layout = tt.ir.LayoutAttr.getLayout(ops.type)
-
-                                    source_node_attrs = [
-                                        graph_builder.KeyValue(
-                                            key="shape", value=str(ops.type.shape)
-                                        ),
-                                        graph_builder.KeyValue(
-                                            key="element_type",
-                                            value=str(ops.type.element_type),
-                                        ),
-                                        graph_builder.KeyValue(
-                                            key="rank", value=str(ops.type.rank)
-                                        ),
-                                        graph_builder.KeyValue(
-                                            key="strides",
-                                            value=array_ref_repr(layout.strides),
-                                        ),
-                                        graph_builder.KeyValue(
-                                            key="Out of Bounds Value",
-                                            value=layout.oobval.name,
-                                        ),
-                                        graph_builder.KeyValue(
-                                            key="Memory Space",
-                                            value=layout.memory_space.name,
-                                        ),
-                                        graph_builder.KeyValue(
-                                            key="Grid Shape",
-                                            value=array_ref_repr(
-                                                layout.grid_attr.shape
-                                            ),
-                                        ),
-                                    ]
-
-                                source_node.outputsMetadata.append(
-                                    graph_builder.MetadataItem(
-                                        id=str(connections[source_node.id]),
-                                        attrs=[
-                                            graph_builder.KeyValue(
-                                                key="__tensor_tag", value=id
-                                            )
-                                        ]
-                                        + source_node_attrs,
-                                    )
-                                )
-
-                                source_node.attrs.extend(source_node_attrs)
-
-                                connections[source_node.id] += 1
-
+        # Convert TTIR to Model Explorer Graphs and Display/Return
+        graph = ttir_to_graph(module, ctx)
         return {"graphs": [graph]}
---
+++ src/tt_adapter/overrides.py
@@ -0,0 +1,7 @@
+# Library for implementing overrides to TTIR Modules
+
+from ttmlir import overrides
+
+
+def overrides_process_me_settings(settings):
+    pass
